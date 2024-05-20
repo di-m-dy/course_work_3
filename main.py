140c124460db832d3ba67be4e076204c902f781c
di-m-dy <dimdy11.11@gmail.com>
2024-05-20 10:59:57 +0200

    check pep and comments

--- main.py
+++ main.py
@@ -11,27 +11,33 @@ def main():
     # en: Load the data / ru: Загрузить данные
     try:
         data = utils.read_json_local(config.DATA_PATH)
+    # en: if the file is empty or does not exist / ru: если файл пустой или не существует
     except FileNotFoundError:
         print("Файл не найден")
         exit(1)
     except json.JSONDecodeError:
         print("Ошибка декодирования JSON")
         exit(1)
+
     # en: Set operations / ru: Установить операции
     operations = utils.set_operations(data)
+
     # en: Filter the data / ru: Отфильтровать данные
     data = utils.filter_data(operations, config.FILTER_KEY, config.STATUS)
+
     # en: Sort the data / ru: Отсортировать данные
     data = utils.sort_data(data, 'date')
-    # en: Print the data / ru: Вывести данные
+
+    # en: Create var of complete data / ru: Создаем переменную сформированных данных
     data = '\n'.join([str(operation) for operation in data])
-    # en: Print the description / ru: Вывести описание
+    # en: Creat var for head of output / ru: Создаем переменную для заголовка вывода
     if config.LIMIT_OPERATIONS == 1:
         last_str = "Последняя операция"
     elif config.LIMIT_OPERATIONS > -1:
         last_str = f"Все операции"
     else:
         last_str = f"Последние операции"
+    # en: Output the data / ru: Выводим данные
     print(f"{last_str} со статусом {config.STATUS}:")
     print('-' * 50)
     print(data)
